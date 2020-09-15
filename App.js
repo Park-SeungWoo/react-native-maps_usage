1a20ad2080f7108abe5b76c5f26d4140e4aca929
ParkSeungWoo <tmd24710@naver.com>
2020-09-15 22:26:46 +0900

    geolocation_use

--- App.js
+++ App.js
@@ -9,29 +9,46 @@ import {
   Dimensions,
 } from 'react-native';
 import MapView, {Marker} from 'react-native-maps';
-// import Geolocation from '@react-native-community/geolocation';
+import Geolocation from '@react-native-community/geolocation';
 
 const pwidth = Dimensions.get('window').width;
 const pheight = Dimensions.get('window').height;
 
 export default class App extends Component {
   state = {
-    lat: null,
-    long: null,
+    lat: 32.1111, // dummy value
+    long: 122.121212, //dummy value
   };
+
+  componentDidMount() {
+    Geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          lat: position.coords.latitude,
+          long: position.coords.longitude,
+        });
+        console.log(this.state.lat, this.state.long);
+      },
+      (error) => {
+        // console.log(error.code, error.message);
+        console.log(error, 'error');
+      },
+    );
+  }
 
   render() {
     return (
       <MapView
         style={styles.map}
-        initialRegion={{
-          latitude: 37.482769399999995,
-          longitude: 126.7496159,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
+        region={{
+          latitude: this.state.lat,
+          longitude: this.state.long,
+          latitudeDelta: 0.9,
+          longitudeDelta: 0.9,
         }}>
         <Marker
-          coordinate={{latitude: 37.482769399999995, longitude: 126.7496159}}
+          draggable
+          coordinate={{latitude: this.state.lat, longitude: this.state.long}}
           title={'sample'}
           description={'sample marker'}
         />
